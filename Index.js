625cd2212ed4734f0904f1e974645883e8f43d48
Rajat-x-Jane <rajattomar406@gmail.com>
2026-03-15 20:55:00 +0530

    Update Index.js

--- Index.js
+++ Index.js
@@ -8,24 +8,29 @@ nav.classList.toggle("open");
 });
 // Contact
 const form = document.getElementById("contact-form");
+const formMessage = document.getElementById("form-message");
 
 form.addEventListener("submit", function(e){
-
-e.preventDefault();
-
-const formData = new FormData(form);
-
-fetch("https://script.google.com/macros/s/AKfycbybmsFLyr8fPmpo77iITi5Qgxz_YcIm9vXBpPcK0hwyIvf0xhzPbgQzkYAjVumDRx2PVA/exec",{
-method:"POST",
-body:formData
-})
-.then(res => res.text())
-.then(data => {
-alert("Message Sent");
-form.reset();
-})
-.catch(err => {
-alert("Error sending message");
-});
-
+	e.preventDefault();
+	const formData = new FormData(form);
+	fetch("https://script.google.com/macros/s/AKfycbybmsFLyr8fPmpo77iITi5Qgxz_YcIm9vXBpPcK0hwyIvf0xhzPbgQzkYAjVumDRx2PVA/exec",{
+		method:"POST",
+		body:formData
+	})
+	.then(res => res.text())
+	.then(data => {
+		formMessage.style.display = "block";
+		formMessage.textContent = "Message Sent! We will contact you soon.";
+		setTimeout(()=>{
+			formMessage.style.display = "none";
+			form.reset();
+		}, 3500);
+	})
+	.catch(err => {
+		formMessage.style.display = "block";
+		formMessage.textContent = "Error sending message. Please try again.";
+		setTimeout(()=>{
+			formMessage.style.display = "none";
+		}, 3500);
+	});
 });
